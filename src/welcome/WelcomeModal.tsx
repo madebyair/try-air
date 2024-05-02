f423039337012c1f8272d50a6e3e6805fbe102fd
Oskar Nizioł <oskarniziol@gmail.com>
2024-05-02 23:14:40 +0200

    welcome text

--- src/welcome/WelcomeModal.tsx
+++ src/welcome/WelcomeModal.tsx
@@ -4,8 +4,10 @@ const WelcomeModal = () => {
     const [ t ] = useTranslation()
 
     return (
-        <div className="w-[800px] h-[500px] bg-slate-300 dark:bg-black rounded-xl dark:text-white">
-            {t("Welcome")}
+        <div className="w-[800px] h-[500px] bg-slate-300 dark:bg-black rounded-xl dark:text-white p-16">
+            <h1 className="text-6xl font-bold dark:text-white">{t("Welcome")}</h1>
+            <p className="w-96 font-bold mt-6">{t("Welcome to Try Air! A web application that will allow you to test Airos directly in your browser.")}</p>
+            <p className="mt-2 w-96">{t("Please remember that the Airos system and this application are still under development and this application may not be up to date with the Airos system interface.")}</p>
         </div>
     )
 }
